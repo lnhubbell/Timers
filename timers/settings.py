a2978079b1fc94794318ca80884efbf725267f57
jefrailey <jcaetof@gmail.com>
2014-11-09 13:03:29 -0800

    Update settings.py
    Add rest_framework and timer to INSTALLED_APPS
    Set database to postregesql

--- timers/settings.py
+++ timers/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'timer',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,8 +60,8 @@ WSGI_APPLICATION = 'timers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'timers',
     }
 }
 
