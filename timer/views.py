99bff161526d5511bd9c4d22807a37a0d1356f97
jefrailey <jcaetof@gmail.com>
2014-11-09 15:54:20 -0800

    Add timer list and detail views to views.py
    Add TimerList a generic ListCreateAPIView to display a list of all timers
    Add TimerDetail a generic RetrieveUpdateDestroyAPIView to display the details of a given timer

--- timer/views.py
+++ timer/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Timer
+from .serializers import TimerSerializer
 
-# Create your views here.
+
+class TimerList(generics.ListCreateAPIView):
+    queryset = Timer.objects.all()
+    serializer_class = TimerSerializer
+
+
+class TimerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Timer.objects.all()
+    serializer_class = TimerSerializer
