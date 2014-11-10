6c577ffaadf51860090174c02a572db0c2f38cfd
jefrailey <jcaetof@gmail.com>
2014-11-10 12:38:16 -0800

    Update timer/views.py with user views

--- timer/views.py
+++ timer/views.py
@@ -1,13 +1,34 @@
-from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
 from .models import Timer
-from .serializers import TimerSerializer
+from .serializers import TimerSerializer, UserSerializer
 
 
 class TimerList(generics.ListCreateAPIView):
     queryset = Timer.objects.all()
     serializer_class = TimerSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def pre_save(self, obj):
+        """Add owner to model prior to saving."""
+        obj.owner = self.request.user
 
 
 class TimerDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Timer.objects.all()
     serializer_class = TimerSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def pre_save(self, obj):
+        """Add owner to model prior to saving."""
+        obj.owner = self.request.user
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
