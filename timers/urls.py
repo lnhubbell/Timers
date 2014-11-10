ab32f1084559421656b07438a26798d6cbbb87f2
jefrailey <jcaetof@gmail.com>
2014-11-10 12:38:47 -0800

    Update timers/urls.py with api login url

--- timers/urls.py
+++ timers/urls.py
@@ -6,3 +6,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('timer.urls')),
 )
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
