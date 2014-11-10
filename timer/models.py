2f09670992d4450be4e1fa8ffea11d5553d220f8
jefrailey <jcaetof@gmail.com>
2014-11-10 12:36:56 -0800

    Update timer/models.py with owner

--- timer/models.py
+++ timer/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Timer(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='timers')
     name = models.CharField(max_length=255)
     seconds = models.PositiveIntegerField(default=0,
                                           verbose_name="Time in Seconds")
