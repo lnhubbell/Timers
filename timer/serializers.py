f85983fba7e2485e98e2366e35f5f24f4f2917be
jefrailey <jcaetof@gmail.com>
2014-11-10 12:37:29 -0800

    Update timer/serializers.py wiht users

--- timer/serializers.py
+++ timer/serializers.py
@@ -1,4 +1,4 @@
-# from django.forms import widgets
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Timer
 
@@ -6,3 +6,12 @@ from .models import Timer
 class TimerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timer
+        owner = serializers.Field(source='owner.username')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    timers = serializers.PrimaryKeyRelatedField(many=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'timers')
