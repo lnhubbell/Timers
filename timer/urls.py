bb01b9735150c5a5de2c5980dc6934d13652ef2a
jefrailey <jcaetof@gmail.com>
2014-11-10 12:37:46 -0800

    Update timer/urls.py with user endpoints

--- timer/urls.py
+++ timer/urls.py
@@ -2,9 +2,18 @@ from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from timer import views
 
-urlpatterns = patterns('',
+# urlpatterns = patterns('',
+#     url(r'^timers/$', views.TimerList.as_view()),
+#     url(r'^timers/(?P<pk>[0-9]+)$', views.TimerDetail.as_view()),
+#     url(r'^users/$', views.UserList.as_view()),
+#     url(r'^users/(?P<pk>[0-9]+)$', views.UserDetail.as_view()),
+# )
+
+urlpatterns = [
     url(r'^timers/$', views.TimerList.as_view()),
     url(r'^timers/(?P<pk>[0-9]+)/$', views.TimerDetail.as_view()),
-)
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+]
 
 urlpatters = format_suffix_patterns(urlpatterns)
