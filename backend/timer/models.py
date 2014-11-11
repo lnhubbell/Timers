c88d038fc3f09a4af0ccbe4b0a641b6671cf469e
jefrailey <jcaetof@gmail.com>
2014-11-11 12:22:52 -0800

    Add backend to master

---
+++ backend/timer/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Timer(models.Model):
+    name = models.CharField(max_length=255)
+    seconds = models.PositiveIntegerField(default=0,
+                                          verbose_name="Time in Seconds")
+
+    def increment_time(self, seconds):
+        self.seconds += seconds
