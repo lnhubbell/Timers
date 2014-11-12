c81a6c7ab8c15701890d585642946c8515666ca4
jefrailey <jcaetof@gmail.com>
2014-11-11 16:35:57 -0800

    Update timer/urls.py
    Remove format_suffix_patterns since it's not being used

--- backend/timer/urls.py
+++ backend/timer/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, url
-from rest_framework.urlpatterns import format_suffix_patterns
 from timer import views
 
 urlpatterns = patterns('',
     url(r'^timers/$', views.TimerList.as_view()),
     url(r'^timers/(?P<pk>[0-9]+)/$', views.TimerDetail.as_view()),
 )
-
-urlpatterns = format_suffix_patterns(urlpatterns)
